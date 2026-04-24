47bf365faec3a07a1cc38d2b862bcfc5f5e96ff5
Ilham Muhammad <ilhamuh97@gmail.com>
2026-04-24 10:55:08 +0200

    add a comment in Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Main Methods here
         //runBasicExercises(scanner);
         //runMixedDataTypes(scanner);
         //runOperatorExercises();
