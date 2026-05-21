d6f7ec3fab70f5a54b3fa6c732efdc5fb938484a
ilhamuh97 <ilhamuh97@gmail.com>
2026-05-21 14:15:52 +0200

    add GET requestParam and PathVariable

--- src/main/java/org/example/neuefische/controller/HelloWorldController.java
+++ src/main/java/org/example/neuefische/controller/HelloWorldController.java
@@ -1,15 +1,20 @@
 package org.example.neuefische.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api/hello")
 public class HelloWorldController {
 
-    @GetMapping("/world")
-    public String helloWorld() {
-        return "Hello World";
+    // GET /api/hello
+    @GetMapping()
+    public String helloWorld(@RequestParam("sort") String sort) {
+        return "Hello World " + sort ;
+    }
+
+    // GET /api/hello/id
+    @GetMapping("/{id}")
+    public String helloWorld2(@PathVariable String id) {
+        return "Hello " + id;
     }
 }
